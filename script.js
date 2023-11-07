ee4e7cb751fd5e4cf1028af38544c626645daedb
Rahmadhani Yusuf <Rahmadhani2202@gmail.com>
2023-11-07 21:00:56 +0700

    skill project

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 // Sticky Navbar
+if (performance.navigation.type === 1) {
+	// Page is refreshed, remove the sticky class
+	let header = document.querySelector('.header');
+	header.classList.remove('sticky');
+}
 
 window.onscroll = () => {
 	let header = document.querySelector('.header');
-	header.classList.toggle('sticky', window.scrollY > 120);
+	if (window.scrollY > 100) {
+		header.classList.add('sticky');
+	} else {
+		header.classList.remove('sticky');
+	}
 };
 
-function responsiveNavBar() {
-	var x = document.getElementById("navbar");
-	if (x.className === "navbar") {
-		x.className += " responsive";
-	} else {
-		x.className = "navbar";
-	}
-}
-
+// navbar Responsive
 document.addEventListener('DOMContentLoaded', function () {
 	const menuIcon = document.querySelector('.menu-icon');
 	const navbar = document.getElementById('navbar');
+	const maxScreenWidth = 768;
 
 	menuIcon.addEventListener('click', function () {
-		if (navbar.style.display === 'block') {
+		if (navbar.style.display === 'flex') {
 			navbar.style.display = 'none';
 		} else {
-			navbar.style.display = 'block';
+			navbar.style.display = 'flex';
 		}
 	});
+
+	function handleScreenWidthChange() {
+		if (window.innerWidth <= maxScreenWidth) {
+			navbar.style.display = 'none';
+		} else {
+			navbar.style.display = 'flex';
+		}
+	}
+
+
+	if (window.matchMedia('(max-width: 768px)').matches) {
+		window.addEventListener('scroll', function () {
+			navbar.style.display = 'none';
+		});
+	}
+	
+	handleScreenWidthChange();
+	window.addEventListener('resize', handleScreenWidthChange);
 });
+
