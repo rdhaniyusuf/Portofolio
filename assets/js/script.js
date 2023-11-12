c725eb7dcc68684dcba121b556f4bd35ed209578
Rahmadhani Yusuf <Rahmadhani2202@gmail.com>
2023-11-12 18:04:36 +0700

    add toast and send message to google spreadsheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,9 +62,37 @@ window.addEventListener('scroll', function () {
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz1vij2gv6CNJoF8_lnphm7PReue3oqNgfwGIzmW0ItULnhG85rfIORg_pZY5pS2KiDOg/exec';
 const form = document.forms['submit-message-google'];
 
+const submitButton = document.getElementById('btn-submit');
+const loadingButton = document.getElementById('btn-loading');
+
+let submitSucces = document.querySelector('.submit-success');
+let submitFail = document.querySelector('.submit-fail')
+
 form.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
+
+  submitButton.style.display = "none";
+  loadingButton.style.display = "flex";
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
+    .then(
+      response => {
+        console.log('Success!', response);
+        submitButton.style.display = "block";
+        loadingButton.style.display = "none";
+        submitSucces.classList.add('show');
+        setTimeout(() => {
+          submitSucces.classList.remove('show');
+        }, 3000);
+        form.reset();
+      })
+    .catch(
+      error => {
+        console.error('Error!', error.message)
+        submitButton.style.display = "block";
+        loadingButton.style.display = "none";
+        submitFail.classList.add('show');
+        setTimeout(() => {
+          submitFail.classList.remove('show');
+        }, 3000);
+      })
+});
