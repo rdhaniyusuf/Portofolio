d1ae7f84c57d956236b909e4ef9b0c43f1c75079
Rahmadhani Yusuf <Rahmadhani2202@gmail.com>
2023-11-12 11:55:11 +0700

    85%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,59 @@
 if (PerformanceNavigationTiming === 1) {
-	// Page is refreshed, remove the sticky class
-	let header = document.querySelector('.header');
+  // Page is refreshed, remove the sticky class
+  let header = document.querySelector('.header');
 
-	header.classList.remove('sticky');
+  header.classList.remove('sticky');
 }
 
 window.onscroll = () => {
-	let header = document.querySelector('.header');
-	if (window.scrollY > 100) {
-		header.classList.add('sticky');
-	
-	} else {
-		header.classList.remove('sticky');
-	}
-};
+  let header = document.querySelector('.header');
+  if (window.scrollY > 100) {
+    header.classList.add('sticky');
 
+  } else {
+    header.classList.remove('sticky');
+  }
+};
 // /**
 //  * navbar toggle
 //  */
 
-// const header = document.querySelector("[nav-navbar]");
-// const navToggleBtn = document.querySelector("[nav-toggle]");
+const navToggle = document.getElementById('navToggle');
+const navbar = document.getElementById('navbar');
 
-// navToggleBtn.addEventListener("click", function () {
-//   header.classList.toggle("nav-active");
-//   this.classList.toggle("active");
-// });
+navToggle.addEventListener('click', function () {
+  navbar.classList.toggle('active');
+});
 
-// /**
-//  * toggle the navbar when click any navbar link
-//  */
+// Get all the navbar items
+const sections = document.querySelectorAll('section');
+const navbarItems = document.querySelectorAll('.nav-navbar .navbar-item');
 
-// const navbarLinks = document.querySelectorAll("[navbar-item]");
+// Add scroll event listener
+window.addEventListener('scroll', function () {
+  let currentSection = null;
 
-// for (let i = 0; i < navbarLinks.length; i++) {
-//   navbarLinks[i].addEventListener("click", function () {
-//     header.classList.toggle("nav-active");
-//     navToggleBtn.classList.toggle("active");
-//   });
-// }
+  sections.forEach(section => {
+    // Calculate the position of each section
+    const sectionTop = section.offsetTop - 90; // Adjust as needed
+    const sectionBottom = sectionTop + section.clientHeight;
+
+    // Check if the user has scrolled to a section
+    if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+      currentSection = section;
+    }
+  });
+
+  // Remove the "active" class from all navbar items
+  navbarItems.forEach(item => {
+    item.classList.remove('active');
+  });
+
+  if (currentSection) {
+    // Get the corresponding navbar item and add the "active" class
+    const correspondingNavItem = document.querySelector(`a[href="#${currentSection.id}"]`);
+    if (correspondingNavItem) {
+      correspondingNavItem.parentElement.classList.add('active');
+    }
+  }
+});
